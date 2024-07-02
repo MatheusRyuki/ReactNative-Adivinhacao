5bb6a654dacabd0cd827d9e0b0f6a3223c1fc670
Matheus Ryuki <matheuskaiya2@gmail.com>
2024-07-02 08:51:23 -0300

    estilização de android e ios da tela inicial

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,10 @@
 import { TextInput, Button, View } from "react-native-web";
 import PrimaryButton from "../components/PrimaryButton";
+import { shadowOffset } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
 const startGameScreen = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -15,6 +16,15 @@ export default startGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
   },
 });

4fc1c9940d6afd0727879fbd7ff7608696a18fb9
Matheus Ryuki <matheuskaiya2@gmail.com>
2024-07-08 09:46:49 -0300

    começando a mexer na tela de jogos

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,24 @@
-import { Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const GameScreen = () => {
-  return <Text>Game Screen!</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Game Screen!</Text>
+      <View>
+        <Text>Game Screen!</Text>
+      </View>
+      <View>
+        <Text>Game Screen!</Text>
+      </View>
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});

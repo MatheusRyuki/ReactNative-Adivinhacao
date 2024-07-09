754567042f7e06ca933a2b39d966ccc8bc5f89a8
Matheus Ryuki <matheuskaiya2@gmail.com>
2024-07-09 09:38:53 -0300

    adicionando e usando fontes personalizadas no react-native

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -9,7 +9,7 @@ export default Title;
 const styles = StyleSheet.create({
   title: {
     fontSize: 18,
-    fontWeight: "bold",
+    fontFamily: "open-sans-bold",
     color: "white",
     textAlign: "center",
     borderWidth: 2,

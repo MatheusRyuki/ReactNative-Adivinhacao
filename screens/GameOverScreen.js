0a1b9ea338c061510649ebb2724851bfa746b3a1
Matheus Ryuki <matheuskaiya2@gmail.com>
2024-07-09 09:48:59 -0300

    adicionando imagens foreground

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,41 @@
-import { Text } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 
 const GameOverScreen = () => {
-  return <Text>game is over</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require("../assets/success.png")} />
+      </View>
+      <View>
+        <Text>
+          Seu celular precisou de X tentativas para acertar seu número Y
+        </Text>
+      </View>
+    </View>
+  );
 };
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {},
+});
